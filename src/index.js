36a9239c7a220e5bb14ba4bc614865228fd966fe
Jake Jepson <jwjepson1@gmail.com>
2022-11-06 01:10:12 -0400

    console log the weather data for a given location

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
 import "./styles.css";
+
+async function getWeather(zip) {
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?zip=${zip},us&appid=f73501bec87ccad60630d02e191c918e`);
+    const data = await response.json();
+    return data;
+}
+
+const zipcode = document.querySelector("#zipcode");
+const searchButton = document.querySelector("#search");
+
+searchButton.addEventListener("click", () => {
+    getWeather(zipcode.value).then((data) => {
+        console.log(data);
+    });
+});
